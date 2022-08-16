ff630bcb009d9657d02d4af6ae4f8ccf23560210
Dan Badham <106093201+dbadham-fr@users.noreply.github.com>
2022-08-16 11:47:02 +0100

    440 OBIE 3.1.9 Read/Write API (#55)
    * 440: Adding OBIE Read/Write specs 3.1.9r7
    
    Issue: https://github.com/SecureBankingAccessToolkit/SecureBankingAccessToolkit/issues/440
    
    * 440: Regenerated data model classes for vrp schema v3.1.9r7
    
    - Previously we supported 3.1.9r3 of VRP (other schemas are still on 3.1.8).
    - Fixed bug in uk.org.openbanking.datamodel.vrp.OBDomesticVRPConsentRequestData.ReadRefundAccountEnum definition where true/false values were being processed rather than Yes/No
    - OBCharge2 / OBDomesticVRPControlParameters only changed to match the output of the codegen (unfortunately this includes some superfluous whitespace being added)
    
    Issue: https://github.com/SecureBankingAccessToolkit/SecureBankingAccessToolkit/issues/440
    
    * 440: Fixing ReadRefundAccountEnum NO value
    
    Issue: https://github.com/SecureBankingAccessToolkit/SecureBankingAccessToolkit/issues/440

--- securebanking-openbanking-uk-obie-datamodel/src/main/java/uk/org/openbanking/datamodel/vrp/OBDomesticVRPConsentRequestData.java
+++ securebanking-openbanking-uk-obie-datamodel/src/main/java/uk/org/openbanking/datamodel/vrp/OBDomesticVRPConsentRequestData.java
@@ -37,7 +37,7 @@ public class OBDomesticVRPConsentRequestData {
     public enum ReadRefundAccountEnum {
         YES("Yes"),
 
-        NO("false");
+        NO("No");
 
         private String value;
 
