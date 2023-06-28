7b8aa910f6df46aab9bad9a1874d40ee87fc198b
Dan Badham <106093201+dbadham-fr@users.noreply.github.com>
2023-06-28 12:13:52 +0100

    1032: Adding FR data model classes for VRP consents (#107)
    FR data model classes for VRP consents are required in order to store them in the RCS Consent Store.
    
    Adding FRDomesticVRPConsentConverters to handle conversion between FR and OB types.
    
    Fixing OBDomesticVRPControlParameters generated code to use the OBSupplementaryData1 type for the supplementaryData field. This is for consistency with other payments which reuse this common type.
    
    Adding OBDomesticVRPControlParametersTest.java
    
    https://github.com/SecureApiGateway/SecureApiGateway/issues/1032

---
+++ secure-api-gateway-ob-uk-common-obie-datamodel/src/test/java/uk/org/openbanking/datamodel/vrp/OBDomesticVRPControlParametersTest.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright © 2020-2022 ForgeRock AS (obst@forgerock.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.org.openbanking.datamodel.vrp;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.lang.reflect.Field;
+
+import org.junit.jupiter.api.Test;
+
+import uk.org.openbanking.datamodel.common.OBSupplementaryData1;
+
+class OBDomesticVRPControlParametersTest {
+
+    /**
+     * Test OBDomesticVRPControlParameters.supplementaryData is the correct type.
+     *
+     * The code-gen by default produces a field of type Object, the generated code has been edited to use the
+     * common OBSupplementaryData1 type.
+     *
+     * If this test fails then update OBDomesticVRPControlParameters.supplementaryData field to be OBSupplementaryData1
+     */
+    @Test
+    void ensureSupplementaryDataFieldIsCorrectType() throws Exception {
+        final Field supplementaryDataField = OBDomesticVRPControlParameters.class.getDeclaredField("supplementaryData");
+        assertEquals(OBSupplementaryData1.class, supplementaryDataField.getType());
+    }
+
+}
