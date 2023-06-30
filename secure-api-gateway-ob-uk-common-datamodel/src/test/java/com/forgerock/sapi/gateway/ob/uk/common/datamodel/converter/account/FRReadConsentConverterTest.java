0fb6ef9718d5644d4ceb3001bcbcdb3801f8ebe4
Dan Badham <dan.badham@forgerock.com>
2023-06-30 12:04:07 +0100

    1023: Refactoring FRAccountConsent into FRReadConsent
    Renamed class to follow naming convention used by other consents in the FR data model.
    
    Refactored class to contain only the OB data model fields.
    
    Added FRAccountAccessConsentConverter + Test
    
    Removing FRAccountAccessConsentTestDataFactory as it is unused.
    
    Removed TestDataFactory
    
    https://github.com/SecureApiGateway/SecureApiGateway/issues/1023

---
+++ secure-api-gateway-ob-uk-common-datamodel/src/test/java/com/forgerock/sapi/gateway/ob/uk/common/datamodel/converter/account/FRReadConsentConverterTest.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright © 2020-2022 ForgeRock AS (obst@forgerock.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.forgerock.sapi.gateway.ob.uk.common.datamodel.converter.account;
+
+import java.util.List;
+
+import org.assertj.core.api.Assertions;
+import org.joda.time.DateTime;
+import org.junit.jupiter.api.Test;
+
+import com.forgerock.sapi.gateway.ob.uk.common.datamodel.account.FRReadConsent;
+
+import uk.org.openbanking.datamodel.account.OBExternalPermissions1Code;
+import uk.org.openbanking.datamodel.account.OBReadConsent1;
+import uk.org.openbanking.datamodel.account.OBReadData1;
+import uk.org.openbanking.datamodel.account.OBRisk2;
+
+class FRReadConsentConverterTest {
+
+    @Test
+    void testConvert() {
+        OBReadConsent1 consent1 = new OBReadConsent1().
+                data(new OBReadData1()
+                        .permissions(List.of(OBExternalPermissions1Code.READACCOUNTSBASIC,
+                                             OBExternalPermissions1Code.READBALANCES))
+                        .expirationDateTime(DateTime.now().plusDays(30))
+                        .transactionToDateTime(DateTime.now())
+                        .transactionFromDateTime(DateTime.now().minusDays(90)))
+                .risk(new OBRisk2());
+
+        final FRReadConsent frConsent = FRReadConsentConverter.toFRReadConsent(consent1);
+        Assertions.assertThat(FRReadConsentConverter.toOBReadConsent1(frConsent)).isEqualTo(consent1);
+    }
+}
