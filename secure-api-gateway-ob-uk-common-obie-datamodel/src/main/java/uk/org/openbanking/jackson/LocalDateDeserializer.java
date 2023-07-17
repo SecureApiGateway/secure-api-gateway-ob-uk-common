8e5197319c248fb722b721e9552a3a071eb606f9
jorgesanchezperez <jorge.sanchez-perez@forgerock.com>
2023-07-17 17:46:20 +0100

    Added changes requested

---
+++ secure-api-gateway-ob-uk-common-obie-datamodel/src/main/java/uk/org/openbanking/jackson/LocalDateDeserializer.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright © 2020-2022 ForgeRock AS (obst@forgerock.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.org.openbanking.jackson;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
+import org.joda.time.LocalDate;
+
+import java.io.IOException;
+
+/**
+ * Common deserializer to parse a json value with format "dd/MM/yyyy" to {@link LocalDate}
+ */
+public class LocalDateDeserializer extends StdDeserializer<LocalDate> {
+    public LocalDateDeserializer() {
+        this(null);
+    }
+
+    public LocalDateDeserializer(Class<?> vc) {
+        super(vc);
+    }
+
+    @Override
+    public LocalDate deserialize(JsonParser jsonParser, DeserializationContext deserializationContext)
+            throws IOException {
+        String date = jsonParser.getText();
+        return LocalDate.parse(date, LocalDateFormat.formatter);
+    }
+}
