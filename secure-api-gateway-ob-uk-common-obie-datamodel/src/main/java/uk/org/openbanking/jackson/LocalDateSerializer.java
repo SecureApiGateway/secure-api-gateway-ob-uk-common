4745a436200126f64501c6859c8f98581a5f7e30
Jorge Sanchez Perez <54277573+jorgesanchezperez@users.noreply.github.com>
2023-07-18 08:26:23 +0100

    1069: Customer info details serialize/deserialize (#120)
    * 1069: Customer info support
    - Added objects to support user account customer info resource
    
    Issue: https://github.com/SecureApiGateway/SecureApiGateway/issues/1069
    
    * Added customer info dto objects to build the request thru components
    
    * 1069: Customer info details serialize/deserialize
    To parse properly the json value birthdate with format "dd/MM/yyyy" it's necessary implement the proper deserializer to parse the text to a LocalDate object that represents the birthDate of the customer.
    - Added new serializer and deserializer for LocalDate joda object.
    
    Issue: https://github.com/SecureApiGateway/SecureApiGateway/issues/1069
    
    * Added changes requested

---
+++ secure-api-gateway-ob-uk-common-obie-datamodel/src/main/java/uk/org/openbanking/jackson/LocalDateSerializer.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright © 2020-2022 ForgeRock AS (obst@forgerock.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.org.openbanking.jackson;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.ser.std.StdSerializer;
+import org.joda.time.LocalDate;
+
+import java.io.IOException;
+
+/**
+ * Common serializer to write {@link LocalDate} formatted as "dd/MM/yyyy"
+ */
+public class LocalDateSerializer extends StdSerializer<LocalDate> {
+
+    public LocalDateSerializer() {
+        this(null);
+    }
+
+    public LocalDateSerializer(Class<LocalDate> t) {
+        super(t);
+    }
+
+    @Override
+    public void serialize(LocalDate localDate, JsonGenerator jsonGenerator, SerializerProvider serializerProvider)
+            throws IOException {
+        jsonGenerator.writeString(LocalDateFormat.formatter.print(localDate));
+    }
+}
