b65b8b970c15a6adc5e9b06708d6a762578ada12
Dan Badham <dan.badham@forgerock.com>
2023-07-18 14:30:28 +0100

    1062: Adding converter to transform FRWriteInternationalConsent back to the OB data-model
    https://github.com/SecureApiGateway/SecureApiGateway/issues/1062

---
+++ secure-api-gateway-ob-uk-common-datamodel/src/test/java/com/forgerock/sapi/gateway/ob/uk/common/datamodel/converter/payment/FRWriteInternationalConsentConverterTest.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright © 2020-2022 ForgeRock AS (obst@forgerock.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.forgerock.sapi.gateway.ob.uk.common.datamodel.converter.payment;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import com.forgerock.sapi.gateway.ob.uk.common.datamodel.payment.FRWriteInternationalConsent;
+
+import uk.org.openbanking.datamodel.payment.OBWriteInternationalConsent5;
+import uk.org.openbanking.testsupport.payment.OBWriteInternationalConsentTestDataFactory;
+
+class FRWriteInternationalConsentConverterTest {
+
+    @Test
+    void testConvert() {
+        // Test converting from OB model to FR and back, verify we got back to the original object
+        final OBWriteInternationalConsent5 originalObConsent = OBWriteInternationalConsentTestDataFactory.aValidOBWriteInternationalConsent5();
+        final FRWriteInternationalConsent frConsent = FRWriteInternationalConsentConverter.toFRWriteInternationalConsent(originalObConsent);
+        assertEquals(originalObConsent, FRWriteInternationalConsentConverter.toOBWriteInternationalConsent5(frConsent));
+    }
+
+}
