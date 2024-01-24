45c35780a612adff76a27a57af089ca3884bc9ed
Dan Badham <106093201+dbadham-fr@users.noreply.github.com>
2024-01-24 16:17:05 +0000

    1243: Re-generating events data model using latest code-gen plugin version (#154)
    Removing Events Callback API types. These types are not in the schema, we do not offer the Callback API externally (there's no IG route) and it is a maintenance burden for something which is not testable.
    
    Configuring the code-gen plugin to use our common data model objects for certain types e.g. Links
    
    https://github.com/SecureApiGateway/SecureApiGateway/issues/1243

--- secure-api-gateway-ob-uk-common-datamodel/src/main/java/com/forgerock/sapi/gateway/ob/uk/common/datamodel/converter/event/FREventPollingConverter.java
+++ secure-api-gateway-ob-uk-common-datamodel/src/main/java/com/forgerock/sapi/gateway/ob/uk/common/datamodel/converter/event/FREventPollingConverter.java
@@ -18,7 +18,7 @@ package com.forgerock.sapi.gateway.ob.uk.common.datamodel.converter.event;
 import com.forgerock.sapi.gateway.ob.uk.common.datamodel.event.FREventPolling;
 import com.forgerock.sapi.gateway.ob.uk.common.datamodel.event.FREventPollingError;
 import uk.org.openbanking.datamodel.event.OBEventPolling1;
-import uk.org.openbanking.datamodel.event.OBEventPolling1SetErrs;
+import uk.org.openbanking.datamodel.event.OBEventPolling1SetErrsValue;
 
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -28,19 +28,19 @@ public class FREventPollingConverter {
     public static FREventPolling toFREventPolling(OBEventPolling1 obEventPolling) {
         return obEventPolling == null ? null : FREventPolling.builder()
                 .maxEvents(obEventPolling.getMaxEvents())
-                .returnImmediately(obEventPolling.isReturnImmediately())
+                .returnImmediately(obEventPolling.getReturnImmediately())
                 .ack(obEventPolling.getAck())
                 .setErrs(toFREventPollingErrors(obEventPolling.getSetErrs()))
                 .build();
     }
 
-    public static Map<String, FREventPollingError> toFREventPollingErrors(Map<String, OBEventPolling1SetErrs> setErrs) {
+    public static Map<String, FREventPollingError> toFREventPollingErrors(Map<String, OBEventPolling1SetErrsValue> setErrs) {
         return setErrs == null ? null : setErrs.entrySet().stream()
                 .collect(Collectors.toMap(Map.Entry::getKey, e -> toFREventPollingError(e.getValue())));
 
     }
 
-    public static FREventPollingError toFREventPollingError(OBEventPolling1SetErrs eventPolling1SetErrs) {
+    public static FREventPollingError toFREventPollingError(OBEventPolling1SetErrsValue eventPolling1SetErrs) {
         return eventPolling1SetErrs == null ? null : FREventPollingError.builder()
                 .error(eventPolling1SetErrs.getErr())
                 .description(eventPolling1SetErrs.getDescription())
