2fb7aa3d675f7d7ed23e5ba855f13cb647a87009
Dan Badham <dan.badham@forgerock.com>
2024-07-23 14:03:34 +0100

    1440: Add 4.0.0 payments data model code
    Code generated from schema: payment-initiation-openapi.yaml
    
    Package: uk.org.openbanking.datamodel.v4.payment
    
    This is a first draft of the generated code, code is being reused from the uk.org.openbanking.datamodel.v3.common package, it is likely that we will want to create a new common package for v4 (and re-generate these common types in case they have been changed).
    
    https://github.com/SecureApiGateway/SecureApiGateway/issues/1440

---
+++ secure-api-gateway-ob-uk-common-obie-datamodel/src/main/java/uk/org/openbanking/datamodel/v4/payment/OBReferredDocumentInformation.java
@@ -0,0 +1,209 @@
+/*
+ * Copyright © 2020-2024 ForgeRock AS (obst@forgerock.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.org.openbanking.datamodel.v4.payment;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import org.joda.time.DateTime;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.annotation.Generated;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Size;
+
+/**
+ * OBReferredDocumentInformation
+ */
+
+@Generated(value = "org.openapitools.codegen.languages.SpringCodegen")
+public class OBReferredDocumentInformation {
+
+    private ExternalDocumentType1Code code;
+
+    private String issuer;
+
+    private String number;
+
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    private DateTime relatedDate;
+
+    @Valid
+    private List<String> lineDetails;
+
+    public OBReferredDocumentInformation code(ExternalDocumentType1Code code) {
+        this.code = code;
+        return this;
+    }
+
+    /**
+     * Get code
+     *
+     * @return code
+     */
+    @Valid
+    @Schema(name = "Code", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
+    @JsonProperty("Code")
+    public ExternalDocumentType1Code getCode() {
+        return code;
+    }
+
+    public void setCode(ExternalDocumentType1Code code) {
+        this.code = code;
+    }
+
+    public OBReferredDocumentInformation issuer(String issuer) {
+        this.issuer = issuer;
+        return this;
+    }
+
+    /**
+     * Get issuer
+     *
+     * @return issuer
+     */
+    @Size(min = 1, max = 140)
+    @Schema(name = "Issuer", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
+    @JsonProperty("Issuer")
+    public String getIssuer() {
+        return issuer;
+    }
+
+    public void setIssuer(String issuer) {
+        this.issuer = issuer;
+    }
+
+    public OBReferredDocumentInformation number(String number) {
+        this.number = number;
+        return this;
+    }
+
+    /**
+     * Get number
+     *
+     * @return number
+     */
+    @Size(min = 1, max = 140)
+    @Schema(name = "Number", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
+    @JsonProperty("Number")
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public OBReferredDocumentInformation relatedDate(DateTime relatedDate) {
+        this.relatedDate = relatedDate;
+        return this;
+    }
+
+    /**
+     * All dates in the JSON payloads are represented in ISO 8601 date-time format.  All date-time fields in responses must include the timezone. An example is below: 2017-04-05T10:43:07+00:00
+     *
+     * @return relatedDate
+     */
+    @Valid
+    @Schema(name = "RelatedDate", description = "All dates in the JSON payloads are represented in ISO 8601 date-time format.  All date-time fields in responses must include the timezone. An example is below: 2017-04-05T10:43:07+00:00", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
+    @JsonProperty("RelatedDate")
+    public DateTime getRelatedDate() {
+        return relatedDate;
+    }
+
+    public void setRelatedDate(DateTime relatedDate) {
+        this.relatedDate = relatedDate;
+    }
+
+    public OBReferredDocumentInformation lineDetails(List<String> lineDetails) {
+        this.lineDetails = lineDetails;
+        return this;
+    }
+
+    public OBReferredDocumentInformation addLineDetailsItem(String lineDetailsItem) {
+        if (this.lineDetails == null) {
+            this.lineDetails = new ArrayList<>();
+        }
+        this.lineDetails.add(lineDetailsItem);
+        return this;
+    }
+
+    /**
+     * Get lineDetails
+     *
+     * @return lineDetails
+     */
+
+    @Schema(name = "LineDetails", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
+    @JsonProperty("LineDetails")
+    public List<String> getLineDetails() {
+        return lineDetails;
+    }
+
+    public void setLineDetails(List<String> lineDetails) {
+        this.lineDetails = lineDetails;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        OBReferredDocumentInformation obReferredDocumentInformation = (OBReferredDocumentInformation) o;
+        return Objects.equals(this.code, obReferredDocumentInformation.code) &&
+                Objects.equals(this.issuer, obReferredDocumentInformation.issuer) &&
+                Objects.equals(this.number, obReferredDocumentInformation.number) &&
+                Objects.equals(this.relatedDate, obReferredDocumentInformation.relatedDate) &&
+                Objects.equals(this.lineDetails, obReferredDocumentInformation.lineDetails);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(code, issuer, number, relatedDate, lineDetails);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("class OBReferredDocumentInformation {\n");
+        sb.append("    code: ").append(toIndentedString(code)).append("\n");
+        sb.append("    issuer: ").append(toIndentedString(issuer)).append("\n");
+        sb.append("    number: ").append(toIndentedString(number)).append("\n");
+        sb.append("    relatedDate: ").append(toIndentedString(relatedDate)).append("\n");
+        sb.append("    lineDetails: ").append(toIndentedString(lineDetails)).append("\n");
+        sb.append("}");
+        return sb.toString();
+    }
+
+    /**
+     * Convert the given object to string with each line indented by 4 spaces
+     * (except the first line).
+     */
+    private String toIndentedString(Object o) {
+        if (o == null) {
+            return "null";
+        }
+        return o.toString().replace("\n", "\n    ");
+    }
+}
+
---
+++ secure-api-gateway-ob-uk-common-obie-datamodel/src/main/java/uk/org/openbanking/datamodel/v4/payment/OBWriteDomesticScheduledResponse5Data.java
@@ -0,0 +1,446 @@
+/*
+ * Copyright © 2020-2024 ForgeRock AS (obst@forgerock.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.org.openbanking.datamodel.v4.payment;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import org.joda.time.DateTime;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonTypeName;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.annotation.Generated;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+/**
+ * OBWriteDomesticScheduledResponse5Data
+ */
+
+@JsonTypeName("OBWriteDomesticScheduledResponse5_Data")
+@Generated(value = "org.openapitools.codegen.languages.SpringCodegen")
+public class OBWriteDomesticScheduledResponse5Data {
+
+    private String domesticScheduledPaymentId;
+
+    private String consentId;
+
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    private DateTime creationDateTime;
+
+    private OBWriteDomesticScheduledResponse5DataStatus status;
+
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    private DateTime statusUpdateDateTime;
+
+    @Valid
+    private List<@Valid OBStatusReason> statusReason;
+
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    private DateTime expectedExecutionDateTime;
+
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    private DateTime expectedSettlementDateTime;
+
+    private OBWriteDomesticResponse5DataRefund refund;
+
+    @Valid
+    private List<@Valid OBWriteDomesticConsentResponse5DataChargesInner> charges;
+
+    private OBWriteDomesticScheduledConsent4DataInitiation initiation;
+
+    private OBWriteDomesticResponse5DataMultiAuthorisation multiAuthorisation;
+
+    private OBCashAccountDebtor4 debtor;
+
+    public OBWriteDomesticScheduledResponse5Data() {
+        super();
+    }
+
+    /**
+     * Constructor with only required parameters
+     */
+    public OBWriteDomesticScheduledResponse5Data(String domesticScheduledPaymentId, String consentId, DateTime creationDateTime, OBWriteDomesticScheduledResponse5DataStatus status, DateTime statusUpdateDateTime, OBWriteDomesticScheduledConsent4DataInitiation initiation) {
+        this.domesticScheduledPaymentId = domesticScheduledPaymentId;
+        this.consentId = consentId;
+        this.creationDateTime = creationDateTime;
+        this.status = status;
+        this.statusUpdateDateTime = statusUpdateDateTime;
+        this.initiation = initiation;
+    }
+
+    public OBWriteDomesticScheduledResponse5Data domesticScheduledPaymentId(String domesticScheduledPaymentId) {
+        this.domesticScheduledPaymentId = domesticScheduledPaymentId;
+        return this;
+    }
+
+    /**
+     * OB: Unique identification as assigned by the ASPSP to uniquely identify the domestic schedule payment resource.
+     *
+     * @return domesticScheduledPaymentId
+     */
+    @NotNull
+    @Size(min = 1, max = 40)
+    @Schema(name = "DomesticScheduledPaymentId", description = "OB: Unique identification as assigned by the ASPSP to uniquely identify the domestic schedule payment resource.", requiredMode = Schema.RequiredMode.REQUIRED)
+    @JsonProperty("DomesticScheduledPaymentId")
+    public String getDomesticScheduledPaymentId() {
+        return domesticScheduledPaymentId;
+    }
+
+    public void setDomesticScheduledPaymentId(String domesticScheduledPaymentId) {
+        this.domesticScheduledPaymentId = domesticScheduledPaymentId;
+    }
+
+    public OBWriteDomesticScheduledResponse5Data consentId(String consentId) {
+        this.consentId = consentId;
+        return this;
+    }
+
+    /**
+     * OB: Unique identification as assigned by the ASPSP to uniquely identify the consent resource.
+     *
+     * @return consentId
+     */
+    @NotNull
+    @Size(min = 1, max = 128)
+    @Schema(name = "ConsentId", description = "OB: Unique identification as assigned by the ASPSP to uniquely identify the consent resource.", requiredMode = Schema.RequiredMode.REQUIRED)
+    @JsonProperty("ConsentId")
+    public String getConsentId() {
+        return consentId;
+    }
+
+    public void setConsentId(String consentId) {
+        this.consentId = consentId;
+    }
+
+    public OBWriteDomesticScheduledResponse5Data creationDateTime(DateTime creationDateTime) {
+        this.creationDateTime = creationDateTime;
+        return this;
+    }
+
+    /**
+     * Date and time at which the message was created.All dates in the JSON payloads are represented in ISO 8601 date-time format.  All date-time fields in responses must include the timezone. An example is below: 2017-04-05T10:43:07+00:00
+     *
+     * @return creationDateTime
+     */
+    @NotNull
+    @Valid
+    @Schema(name = "CreationDateTime", description = "Date and time at which the message was created.All dates in the JSON payloads are represented in ISO 8601 date-time format.  All date-time fields in responses must include the timezone. An example is below: 2017-04-05T10:43:07+00:00", requiredMode = Schema.RequiredMode.REQUIRED)
+    @JsonProperty("CreationDateTime")
+    public DateTime getCreationDateTime() {
+        return creationDateTime;
+    }
+
+    public void setCreationDateTime(DateTime creationDateTime) {
+        this.creationDateTime = creationDateTime;
+    }
+
+    public OBWriteDomesticScheduledResponse5Data status(OBWriteDomesticScheduledResponse5DataStatus status) {
+        this.status = status;
+        return this;
+    }
+
+    /**
+     * Get status
+     *
+     * @return status
+     */
+    @NotNull
+    @Valid
+    @Schema(name = "Status", requiredMode = Schema.RequiredMode.REQUIRED)
+    @JsonProperty("Status")
+    public OBWriteDomesticScheduledResponse5DataStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(OBWriteDomesticScheduledResponse5DataStatus status) {
+        this.status = status;
+    }
+
+    public OBWriteDomesticScheduledResponse5Data statusUpdateDateTime(DateTime statusUpdateDateTime) {
+        this.statusUpdateDateTime = statusUpdateDateTime;
+        return this;
+    }
+
+    /**
+     * Date and time at which the resource status was updated.All dates in the JSON payloads are represented in ISO 8601 date-time format.  All date-time fields in responses must include the timezone. An example is below: 2017-04-05T10:43:07+00:00
+     *
+     * @return statusUpdateDateTime
+     */
+    @NotNull
+    @Valid
+    @Schema(name = "StatusUpdateDateTime", description = "Date and time at which the resource status was updated.All dates in the JSON payloads are represented in ISO 8601 date-time format.  All date-time fields in responses must include the timezone. An example is below: 2017-04-05T10:43:07+00:00", requiredMode = Schema.RequiredMode.REQUIRED)
+    @JsonProperty("StatusUpdateDateTime")
+    public DateTime getStatusUpdateDateTime() {
+        return statusUpdateDateTime;
+    }
+
+    public void setStatusUpdateDateTime(DateTime statusUpdateDateTime) {
+        this.statusUpdateDateTime = statusUpdateDateTime;
+    }
+
+    public OBWriteDomesticScheduledResponse5Data statusReason(List<@Valid OBStatusReason> statusReason) {
+        this.statusReason = statusReason;
+        return this;
+    }
+
+    public OBWriteDomesticScheduledResponse5Data addStatusReasonItem(OBStatusReason statusReasonItem) {
+        if (this.statusReason == null) {
+            this.statusReason = new ArrayList<>();
+        }
+        this.statusReason.add(statusReasonItem);
+        return this;
+    }
+
+    /**
+     * Get statusReason
+     *
+     * @return statusReason
+     */
+    @Valid
+    @Schema(name = "StatusReason", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
+    @JsonProperty("StatusReason")
+    public List<@Valid OBStatusReason> getStatusReason() {
+        return statusReason;
+    }
+
+    public void setStatusReason(List<@Valid OBStatusReason> statusReason) {
+        this.statusReason = statusReason;
+    }
+
+    public OBWriteDomesticScheduledResponse5Data expectedExecutionDateTime(DateTime expectedExecutionDateTime) {
+        this.expectedExecutionDateTime = expectedExecutionDateTime;
+        return this;
+    }
+
+    /**
+     * Expected execution date and time for the payment resource.All dates in the JSON payloads are represented in ISO 8601 date-time format.  All date-time fields in responses must include the timezone. An example is below: 2017-04-05T10:43:07+00:00
+     *
+     * @return expectedExecutionDateTime
+     */
+    @Valid
+    @Schema(name = "ExpectedExecutionDateTime", description = "Expected execution date and time for the payment resource.All dates in the JSON payloads are represented in ISO 8601 date-time format.  All date-time fields in responses must include the timezone. An example is below: 2017-04-05T10:43:07+00:00", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
+    @JsonProperty("ExpectedExecutionDateTime")
+    public DateTime getExpectedExecutionDateTime() {
+        return expectedExecutionDateTime;
+    }
+
+    public void setExpectedExecutionDateTime(DateTime expectedExecutionDateTime) {
+        this.expectedExecutionDateTime = expectedExecutionDateTime;
+    }
+
+    public OBWriteDomesticScheduledResponse5Data expectedSettlementDateTime(DateTime expectedSettlementDateTime) {
+        this.expectedSettlementDateTime = expectedSettlementDateTime;
+        return this;
+    }
+
+    /**
+     * Expected settlement date and time for the payment resource.All dates in the JSON payloads are represented in ISO 8601 date-time format.  All date-time fields in responses must include the timezone. An example is below: 2017-04-05T10:43:07+00:00
+     *
+     * @return expectedSettlementDateTime
+     */
+    @Valid
+    @Schema(name = "ExpectedSettlementDateTime", description = "Expected settlement date and time for the payment resource.All dates in the JSON payloads are represented in ISO 8601 date-time format.  All date-time fields in responses must include the timezone. An example is below: 2017-04-05T10:43:07+00:00", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
+    @JsonProperty("ExpectedSettlementDateTime")
+    public DateTime getExpectedSettlementDateTime() {
+        return expectedSettlementDateTime;
+    }
+
+    public void setExpectedSettlementDateTime(DateTime expectedSettlementDateTime) {
+        this.expectedSettlementDateTime = expectedSettlementDateTime;
+    }
+
+    public OBWriteDomesticScheduledResponse5Data refund(OBWriteDomesticResponse5DataRefund refund) {
+        this.refund = refund;
+        return this;
+    }
+
+    /**
+     * Get refund
+     *
+     * @return refund
+     */
+    @Valid
+    @Schema(name = "Refund", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
+    @JsonProperty("Refund")
+    public OBWriteDomesticResponse5DataRefund getRefund() {
+        return refund;
+    }
+
+    public void setRefund(OBWriteDomesticResponse5DataRefund refund) {
+        this.refund = refund;
+    }
+
+    public OBWriteDomesticScheduledResponse5Data charges(List<@Valid OBWriteDomesticConsentResponse5DataChargesInner> charges) {
+        this.charges = charges;
+        return this;
+    }
+
+    public OBWriteDomesticScheduledResponse5Data addChargesItem(OBWriteDomesticConsentResponse5DataChargesInner chargesItem) {
+        if (this.charges == null) {
+            this.charges = new ArrayList<>();
+        }
+        this.charges.add(chargesItem);
+        return this;
+    }
+
+    /**
+     * Get charges
+     *
+     * @return charges
+     */
+    @Valid
+    @Schema(name = "Charges", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
+    @JsonProperty("Charges")
+    public List<@Valid OBWriteDomesticConsentResponse5DataChargesInner> getCharges() {
+        return charges;
+    }
+
+    public void setCharges(List<@Valid OBWriteDomesticConsentResponse5DataChargesInner> charges) {
+        this.charges = charges;
+    }
+
+    public OBWriteDomesticScheduledResponse5Data initiation(OBWriteDomesticScheduledConsent4DataInitiation initiation) {
+        this.initiation = initiation;
+        return this;
+    }
+
+    /**
+     * Get initiation
+     *
+     * @return initiation
+     */
+    @NotNull
+    @Valid
+    @Schema(name = "Initiation", requiredMode = Schema.RequiredMode.REQUIRED)
+    @JsonProperty("Initiation")
+    public OBWriteDomesticScheduledConsent4DataInitiation getInitiation() {
+        return initiation;
+    }
+
+    public void setInitiation(OBWriteDomesticScheduledConsent4DataInitiation initiation) {
+        this.initiation = initiation;
+    }
+
+    public OBWriteDomesticScheduledResponse5Data multiAuthorisation(OBWriteDomesticResponse5DataMultiAuthorisation multiAuthorisation) {
+        this.multiAuthorisation = multiAuthorisation;
+        return this;
+    }
+
+    /**
+     * Get multiAuthorisation
+     *
+     * @return multiAuthorisation
+     */
+    @Valid
+    @Schema(name = "MultiAuthorisation", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
+    @JsonProperty("MultiAuthorisation")
+    public OBWriteDomesticResponse5DataMultiAuthorisation getMultiAuthorisation() {
+        return multiAuthorisation;
+    }
+
+    public void setMultiAuthorisation(OBWriteDomesticResponse5DataMultiAuthorisation multiAuthorisation) {
+        this.multiAuthorisation = multiAuthorisation;
+    }
+
+    public OBWriteDomesticScheduledResponse5Data debtor(OBCashAccountDebtor4 debtor) {
+        this.debtor = debtor;
+        return this;
+    }
+
+    /**
+     * Get debtor
+     *
+     * @return debtor
+     */
+    @Valid
+    @Schema(name = "Debtor", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
+    @JsonProperty("Debtor")
+    public OBCashAccountDebtor4 getDebtor() {
+        return debtor;
+    }
+
+    public void setDebtor(OBCashAccountDebtor4 debtor) {
+        this.debtor = debtor;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        OBWriteDomesticScheduledResponse5Data obWriteDomesticScheduledResponse5Data = (OBWriteDomesticScheduledResponse5Data) o;
+        return Objects.equals(this.domesticScheduledPaymentId, obWriteDomesticScheduledResponse5Data.domesticScheduledPaymentId) &&
+                Objects.equals(this.consentId, obWriteDomesticScheduledResponse5Data.consentId) &&
+                Objects.equals(this.creationDateTime, obWriteDomesticScheduledResponse5Data.creationDateTime) &&
+                Objects.equals(this.status, obWriteDomesticScheduledResponse5Data.status) &&
+                Objects.equals(this.statusUpdateDateTime, obWriteDomesticScheduledResponse5Data.statusUpdateDateTime) &&
+                Objects.equals(this.statusReason, obWriteDomesticScheduledResponse5Data.statusReason) &&
+                Objects.equals(this.expectedExecutionDateTime, obWriteDomesticScheduledResponse5Data.expectedExecutionDateTime) &&
+                Objects.equals(this.expectedSettlementDateTime, obWriteDomesticScheduledResponse5Data.expectedSettlementDateTime) &&
+                Objects.equals(this.refund, obWriteDomesticScheduledResponse5Data.refund) &&
+                Objects.equals(this.charges, obWriteDomesticScheduledResponse5Data.charges) &&
+                Objects.equals(this.initiation, obWriteDomesticScheduledResponse5Data.initiation) &&
+                Objects.equals(this.multiAuthorisation, obWriteDomesticScheduledResponse5Data.multiAuthorisation) &&
+                Objects.equals(this.debtor, obWriteDomesticScheduledResponse5Data.debtor);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(domesticScheduledPaymentId, consentId, creationDateTime, status, statusUpdateDateTime, statusReason, expectedExecutionDateTime, expectedSettlementDateTime, refund, charges, initiation, multiAuthorisation, debtor);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("class OBWriteDomesticScheduledResponse5Data {\n");
+        sb.append("    domesticScheduledPaymentId: ").append(toIndentedString(domesticScheduledPaymentId)).append("\n");
+        sb.append("    consentId: ").append(toIndentedString(consentId)).append("\n");
+        sb.append("    creationDateTime: ").append(toIndentedString(creationDateTime)).append("\n");
+        sb.append("    status: ").append(toIndentedString(status)).append("\n");
+        sb.append("    statusUpdateDateTime: ").append(toIndentedString(statusUpdateDateTime)).append("\n");
+        sb.append("    statusReason: ").append(toIndentedString(statusReason)).append("\n");
+        sb.append("    expectedExecutionDateTime: ").append(toIndentedString(expectedExecutionDateTime)).append("\n");
+        sb.append("    expectedSettlementDateTime: ").append(toIndentedString(expectedSettlementDateTime)).append("\n");
+        sb.append("    refund: ").append(toIndentedString(refund)).append("\n");
+        sb.append("    charges: ").append(toIndentedString(charges)).append("\n");
+        sb.append("    initiation: ").append(toIndentedString(initiation)).append("\n");
+        sb.append("    multiAuthorisation: ").append(toIndentedString(multiAuthorisation)).append("\n");
+        sb.append("    debtor: ").append(toIndentedString(debtor)).append("\n");
+        sb.append("}");
+        return sb.toString();
+    }
+
+    /**
+     * Convert the given object to string with each line indented by 4 spaces
+     * (except the first line).
+     */
+    private String toIndentedString(Object o) {
+        if (o == null) {
+            return "null";
+        }
+        return o.toString().replace("\n", "\n    ");
+    }
+}
+
