60ae722e774225e5f8aaa2c905b6acc6591100d2
Dan Badham <106093201+dbadham-fr@users.noreply.github.com>
2024-07-29 09:17:26 +0100

    1440: Add v4 confirmation of funds data model (#194)
    Code generated from confirmation-funds-openapi.yaml using OB v4 schema into pkg: uk.org.openbanking.datamodel.v4.fund
    
    Adjusted code gen config to create OBFundsConfirmationConsentStatus as a top-level class rather than having it nested.
    
    https://github.com/SecureApiGateway/SecureApiGateway/issues/1440

---
+++ secure-api-gateway-ob-uk-common-obie-datamodel/src/main/java/uk/org/openbanking/datamodel/v4/fund/OBFundsConfirmationConsent1DataDebtorAccount.java
@@ -0,0 +1,211 @@
+/*
+ * Copyright © 2020-2024 ForgeRock AS (obst@forgerock.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.org.openbanking.datamodel.v4.fund;
+
+import java.util.Objects;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonTypeName;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.annotation.Generated;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+/**
+ * Unambiguous identification of the account of the debtor to which a confirmation of funds consent will be applied.
+ */
+
+@Schema(name = "OBFundsConfirmationConsent1_Data_DebtorAccount", description = "Unambiguous identification of the account of the debtor to which a confirmation of funds consent will be applied.")
+@JsonTypeName("OBFundsConfirmationConsent1_Data_DebtorAccount")
+@Generated(value = "org.openapitools.codegen.languages.SpringCodegen")
+public class OBFundsConfirmationConsent1DataDebtorAccount {
+
+    private String schemeName;
+
+    private String identification;
+
+    private String name;
+
+    private String secondaryIdentification;
+
+    private OBProxy1 proxy;
+
+    public OBFundsConfirmationConsent1DataDebtorAccount() {
+        super();
+    }
+
+    /**
+     * Constructor with only required parameters
+     */
+    public OBFundsConfirmationConsent1DataDebtorAccount(String schemeName, String identification) {
+        this.schemeName = schemeName;
+        this.identification = identification;
+    }
+
+    public OBFundsConfirmationConsent1DataDebtorAccount schemeName(String schemeName) {
+        this.schemeName = schemeName;
+        return this;
+    }
+
+    /**
+     * Name of the identification scheme, in a coded form as published in an external list.
+     *
+     * @return schemeName
+     */
+    @NotNull
+    @Schema(name = "SchemeName", description = "Name of the identification scheme, in a coded form as published in an external list.", requiredMode = Schema.RequiredMode.REQUIRED)
+    @JsonProperty("SchemeName")
+    public String getSchemeName() {
+        return schemeName;
+    }
+
+    public void setSchemeName(String schemeName) {
+        this.schemeName = schemeName;
+    }
+
+    public OBFundsConfirmationConsent1DataDebtorAccount identification(String identification) {
+        this.identification = identification;
+        return this;
+    }
+
+    /**
+     * Identification assigned by an institution to identify an account. This identification is known by the account owner.
+     *
+     * @return identification
+     */
+    @NotNull
+    @Size(min = 1, max = 256)
+    @Schema(name = "Identification", description = "Identification assigned by an institution to identify an account. This identification is known by the account owner.", requiredMode = Schema.RequiredMode.REQUIRED)
+    @JsonProperty("Identification")
+    public String getIdentification() {
+        return identification;
+    }
+
+    public void setIdentification(String identification) {
+        this.identification = identification;
+    }
+
+    public OBFundsConfirmationConsent1DataDebtorAccount name(String name) {
+        this.name = name;
+        return this;
+    }
+
+    /**
+     * Name of the account, as assigned by the account servicing institution. Usage: The account name is the name or names of the account owner(s) represented at an account level. The account name is not the product name or the nickname of the account.
+     *
+     * @return name
+     */
+    @Size(min = 1, max = 350)
+    @Schema(name = "Name", description = "Name of the account, as assigned by the account servicing institution. Usage: The account name is the name or names of the account owner(s) represented at an account level. The account name is not the product name or the nickname of the account.", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
+    @JsonProperty("Name")
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public OBFundsConfirmationConsent1DataDebtorAccount secondaryIdentification(String secondaryIdentification) {
+        this.secondaryIdentification = secondaryIdentification;
+        return this;
+    }
+
+    /**
+     * This is secondary identification of the account, as assigned by the account servicing institution.  This can be used by building societies to additionally identify accounts with a roll number (in addition to a sort code and account number combination).
+     *
+     * @return secondaryIdentification
+     */
+    @Size(min = 1, max = 34)
+    @Schema(name = "SecondaryIdentification", description = "This is secondary identification of the account, as assigned by the account servicing institution.  This can be used by building societies to additionally identify accounts with a roll number (in addition to a sort code and account number combination).", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
+    @JsonProperty("SecondaryIdentification")
+    public String getSecondaryIdentification() {
+        return secondaryIdentification;
+    }
+
+    public void setSecondaryIdentification(String secondaryIdentification) {
+        this.secondaryIdentification = secondaryIdentification;
+    }
+
+    public OBFundsConfirmationConsent1DataDebtorAccount proxy(OBProxy1 proxy) {
+        this.proxy = proxy;
+        return this;
+    }
+
+    /**
+     * Get proxy
+     *
+     * @return proxy
+     */
+    @Valid
+    @Schema(name = "Proxy", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
+    @JsonProperty("Proxy")
+    public OBProxy1 getProxy() {
+        return proxy;
+    }
+
+    public void setProxy(OBProxy1 proxy) {
+        this.proxy = proxy;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        OBFundsConfirmationConsent1DataDebtorAccount obFundsConfirmationConsent1DataDebtorAccount = (OBFundsConfirmationConsent1DataDebtorAccount) o;
+        return Objects.equals(this.schemeName, obFundsConfirmationConsent1DataDebtorAccount.schemeName) &&
+                Objects.equals(this.identification, obFundsConfirmationConsent1DataDebtorAccount.identification) &&
+                Objects.equals(this.name, obFundsConfirmationConsent1DataDebtorAccount.name) &&
+                Objects.equals(this.secondaryIdentification, obFundsConfirmationConsent1DataDebtorAccount.secondaryIdentification) &&
+                Objects.equals(this.proxy, obFundsConfirmationConsent1DataDebtorAccount.proxy);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(schemeName, identification, name, secondaryIdentification, proxy);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("class OBFundsConfirmationConsent1DataDebtorAccount {\n");
+        sb.append("    schemeName: ").append(toIndentedString(schemeName)).append("\n");
+        sb.append("    identification: ").append(toIndentedString(identification)).append("\n");
+        sb.append("    name: ").append(toIndentedString(name)).append("\n");
+        sb.append("    secondaryIdentification: ").append(toIndentedString(secondaryIdentification)).append("\n");
+        sb.append("    proxy: ").append(toIndentedString(proxy)).append("\n");
+        sb.append("}");
+        return sb.toString();
+    }
+
+    /**
+     * Convert the given object to string with each line indented by 4 spaces
+     * (except the first line).
+     */
+    private String toIndentedString(Object o) {
+        if (o == null) {
+            return "null";
+        }
+        return o.toString().replace("\n", "\n    ");
+    }
+}
+
